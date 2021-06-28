10a57b910a49f29ed2b6911763c56850bb9589eb
hwc9169 <hwc9169@gmail.com>
2021-06-28 22:41:22 +0900

    update authenticator logic

--- middleware/authenticator.go
+++ middleware/authenticator.go
@@ -4,17 +4,16 @@ import (
 	"context"
 	"fmt"
 	"github.com/auto-check/common-module/jwt"
-	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc/metadata"
 	"runtime/debug"
 )
 
 func Authenticator(ctx context.Context) (context.Context, error) {
-	token, err := grpc_auth.AuthFromMD(ctx, "bearer")
-	if err != nil {
-		log.Errorf(fmt.Sprintf("Error %s\n%s", err, debug.Stack()))
-		return nil, err
-	}
+	headers, _ := metadata.FromIncomingContext(ctx)
+	log.Println(headers)
+
+	token := headers.Get("authorization")[1]
 
 	id, err := jwt.ParseStudentIDFromToken(token)
 	if err != nil {
